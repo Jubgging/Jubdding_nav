3b704393f15df461bee8bab172a1d0fcc94d8015
cobinding <cntpqls0218@gmail.com>
2022-11-26 00:48:19 +0900

    firebase

--- app/src/main/java/com/example/jubgging_nav/MainFragment.kt
+++ app/src/main/java/com/example/jubgging_nav/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.jubgging_nav.databinding.FragmentMainBinding
 import com.example.jubgging_nav.databinding.FragmentRecordBinding
 
@@ -25,4 +26,16 @@ class MainFragment : Fragment() {
         mBinding = null
         super.onDestroyView()
     }
+
+    //binding되고 나서 불리는 함수
+    // 버튼 눌려서 어떻게 할 지 설정
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding?.btnPlogging?.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_PloggingMapsFragment)
+
+        }
+    }
 }
+
