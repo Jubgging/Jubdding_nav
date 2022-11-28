34e1d3bb34642443b71db6e333fc81baf210e55e
cobinding <cntpqls0218@gmail.com>
2022-11-28 14:18:57 +0900

    time

--- app/src/main/java/com/example/jubgging_nav/PloggingMapsFragment.kt
+++ app/src/main/java/com/example/jubgging_nav/PloggingMapsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.jubgging_nav
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,20 +13,29 @@ import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.Timer
+import kotlin.concurrent.timer
 
 
 class PloggingMapsFragment : Fragment(), OnMapReadyCallback {
 
+    // onAttach() 콜백메서드에서 Context를 MainActivity로 형변환하여 할당
 
+     lateinit var mainActivity: MainActivity // context를 할당할 변수를 프로퍼티로 선언
+     override fun onAttach(context: Context) {
+         super.onAttach(context)
+         mainActivity = context as MainActivity
+         // mainActivity.runOnUIThread 이런식으로 사용하면 된다..
+     }
+
+    private var time = 0
+    private var timerTask:Timer? = null
 
     lateinit var binding: FragmentMapsBinding
     lateinit var mapView: MapView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 
 
@@ -35,7 +45,6 @@ class PloggingMapsFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMapsBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
@@ -46,8 +55,22 @@ class PloggingMapsFragment : Fragment(), OnMapReadyCallback {
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
 
+        setFABClickEvent()
     }
 
+    private fun setFABClickEvent() {
+        binding.fab.setOnClickListener{
+            timerTask = timer(period = 10){
+                time++
+                val sec = time / 100
+                val milli = time % 100
+                mainActivity.runOnUiThread{
+                    binding.txtSet.text ="$sec"
+                    binding.txtMill.text = "$milli"
+                }
+            }
+        }
+    }
     override fun onMapReady(googleMap: GoogleMap) {
         // 서울 좌표 입력 후 카메라를 서울로 이동 시키고 10f 수준으로 줌시킴
         val seoul = LatLng(37.566, 126.978)
@@ -91,6 +114,24 @@ class PloggingMapsFragment : Fragment(), OnMapReadyCallback {
         mapView.onLowMemory()
         super.onLowMemory()
     }
+
+    /*private fun start(){ // 스톱워치 시작 btn
+        binding.fab.setImageResource(R.drawable.ic_baseline_pause_circle_24)
+        timerTask = timer(period = 10){
+            time++
+            val sec = time / 100
+            val milli = time % 100
+            mainActivity.runOnUiThread{
+                binding.txtSet.text ="$sec"
+                binding.txtMill.text = "$milli"
+            }
+        }
+    }
+
+    private fun pause(){
+        binding.fab.setImageResource(R.drawable.ic_baseline_play_circle_24)
+        timerTask?.cancel()
+    }*/
 
 
 }
